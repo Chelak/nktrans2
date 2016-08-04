b0d678200fcd3444b9e5487d12d9dec2d0ab44f2
scelac <celaserg@gmail.com>
2016-08-04 15:23:22 +0300

    Initial commit

---
+++ src/main/java/com/nktrans/NkTransApplication.java
@@ -0,0 +1,14 @@
+package com.nktrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class NkTransApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NkTransApplication.class, args);
+	}
+}
---
+++ src/main/java/com/nktrans/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.nktrans.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by scelac on 8/4/16.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String helloWorld(Model model) {
+        model.addAttribute("russian", "Добрый день");
+        return "hello-world";
+    }
+}
