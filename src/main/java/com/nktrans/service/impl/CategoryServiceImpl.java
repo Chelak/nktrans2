f84fe696c38f6f548ba2d0182e5df18e119cc6d6
Chelak <celaserg@gmail.com>
2016-08-09 10:53:59 +0000

    annotate service and transactional

--- src/main/java/com/nktrans/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/nktrans/service/impl/CategoryServiceImpl.java
@@ -4,32 +4,39 @@ import com.nktrans.entity.Category;
 import com.nktrans.repository.CategoryRepository;
 import com.nktrans.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Created by scelac on 8/5/16.
  */
+@Service
 public class CategoryServiceImpl implements CategoryService {
    @Autowired
    CategoryRepository categoryRepository;
 
     @Override
+    @Transactional
     public Category findOne(Long categoryId) {
         return categoryRepository.findOne(categoryId);
     }
 
     @Override
+    @Transactional
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
 
     @Override
+    @Transactional
     public void save(Category category) {
         categoryRepository.save(category);
     }
 
     @Override
+    @Transactional
     public void deleteById(Long categoryId) {
             categoryRepository.delete(categoryId);
     }
--- src/main/java/com/nktrans/service/impl/ItemServiceImpl.java
+++ src/main/java/com/nktrans/service/impl/ItemServiceImpl.java
@@ -4,37 +4,46 @@ import com.nktrans.entity.Item;
 import com.nktrans.repository.ItemRepository;
 import com.nktrans.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * Created by scelac on 8/5/16.
  */
+@Service
 public class ItemServiceImpl implements ItemService {
 
     @Autowired
     ItemRepository itemRepository;
+
     @Override
+    @Transactional
     public Item findOne(Long itemId) {
         return null;
     }
 
     @Override
+    @Transactional
     public List<Item> findAll() {
         return null;
     }
 
     @Override
+    @Transactional
     public List<Item> findByCategoryId(Long categoryId) {
         return null;
     }
 
     @Override
+    @Transactional
     public void save(Item item) {
 
     }
 
     @Override
+    @Transactional
     public void deleteById(Long itemId) {
 
     }
